58bbb851a0f27698db7996e2d4634996d692e8d3
huangshangbin <1661729343@qq.com>
2020-03-24 09:43:39 +0800

    script push

---
+++ delete.bat
@@ -0,0 +1,2 @@
+rd /s /q MysqlOrm\x64
+rd /s /q MysqlOrm\MysqlOrm\x64
--- push_include.bat
+++ push_include.bat
@@ -1,3 +1,6 @@
+#删除编译生成的文件
+rd /s /q MysqlOrm\x64
+rd /s /q MysqlOrm\MysqlOrm\x64
 git add .
 git commit -m "script push"
 git push origin master
